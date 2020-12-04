1f237a6296bd5735f688da4ebe4c1cb6631c4483
Elias Leinenweber <elias.leinenweber@etu.unistra.fr>
2020-12-04 16:52:49 +0100

    Amélioré classe Main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,49 +1,78 @@
-#include <iostream>
-
+#include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
+#include <SFML/Window.hpp>
 
 #include "model/Actor.h"
 #include "view/GameWindow.h"
 
+#define RESOURCES_FOLDER "resources/"
+
+/**
+ * Point d'entrée principal.
+ */
 int
 main()
 {
-    // crée la fenêtre
-    sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
+    /* Crée la fenêtre. */
+    sf::RenderWindow window(sf::VideoMode(800, 600),
+                            "Headmaster Simulator",
+                            sf::Style::Default,
+                            sf::ContextSettings(24, 0, 0, 4, 6));
     window.setVerticalSyncEnabled(false);
 
-    // activation de la fenêtre
+    /* Activation de la fenêtre. */
     window.setActive(true);
 
-    // chargement des ressources, initialisation des états OpenGL, ...
-    sf::Clock clock;
-    float lastTime = 0;
+    /* Chargement des ressources, initialisation des états OpenGL... */
+    sf::Font font;
+    if (!font.loadFromFile(RESOURCES_FOLDER "Roboto-Light.ttf"))
+        return EXIT_FAILURE;
+    sf::Text text;
+    text.setFont(font);
+    text.setString("Barre oblique");
+    text.setCharacterSize(24);
+    text.setFillColor(sf::Color::White);
+    text.setStyle(sf::Text::Bold);
+    text.move(50.0f, 250.0f);
 
-    // la boucle principale
+    /* Démarre le chronomètre. */
+    sf::Clock clock;
+
+    sf::RectangleShape line(sf::Vector2f(150, 5));
+    line.rotate(-45);
+    line.move(50.0f, 200.0f);
+
+    /* La boucle de jeu principale. */
     bool running = true;
     while (running) {
-        // gestion des évènements
+        /* Gestion des évènements. */
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
-                // on stoppe le programme
+                /* Stoppe le programme. */
                 running = false;
             } else if (event.type == sf::Event::Resized) {
-                // on ajuste le viewport lorsque la fenêtre est redimensionnée
+                /* Ajuste le viewport lorsque la fenêtre est redimensionnée. */
                 glViewport(0, 0, event.size.width, event.size.height);
             }
         }
 
-        // effacement les tampons de couleur/profondeur
+        /* Effacement des tampons de couleur et de profondeur. */
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // dessin...
+        /* Dessin. */
+        window.draw(line);
+        window.draw(text);
 
-        // termine la trame courante (en interne, échange les deux tampons de rendu)
+        /*
+         * Termine la trame courante (en interne, échange les deux tampons de
+         * rendu).
+         */
         window.display();
     }
 
-    // libération des ressources...
+    /* Libération des ressources. */
+    window.setActive(false);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
