66db3ece1ce021af983c3369beb03d8605553c11
Elias Leinenweber <elias.leinenweber@etu.unistra.fr>
2020-12-10 23:30:15 +0100

    Mis en place structure de Popup quand on clique sur un Actor

--- src/model/Actor.cpp
+++ src/model/Actor.cpp
@@ -1,8 +1,11 @@
+#include <string>
+
 #include <SFML/Graphics.hpp>
 
 #include "imgui/imgui-SFML.h"
 #include "imgui/imgui.h"
 
+#include "controller/Game.h"
 #include "model/Actor.h"
 
 Actor::Actor(const std::string &firstname, const std::string &surname,
@@ -16,6 +19,20 @@ Actor::Actor(const std::string &firstname, const std::string &surname,
 void
 Actor::render() const
 {
-    ImGui::ImageButton(
-      texture, ImVec2(texture.getSize().x / 2, texture.getSize().y / 2));
+    const char *str_id = std::to_string(Game::getComponentID()).c_str();
+
+    if (ImGui::ImageButton(
+          texture, ImVec2(texture.getSize().x / 2, texture.getSize().y / 2)))
+        ImGui::OpenPopup(str_id);
+    if (ImGui::BeginPopup(str_id)) {
+        if (ImGui::MenuItem("Afficher fiche information")) {
+        }
+        if (ImGui::MenuItem("Interroger")) {
+        }
+        if (ImGui::MenuItem("Lancer une craie")) {
+        }
+        if (ImGui::MenuItem("Envoyer au coin")) {
+        }
+        ImGui::EndPopup();
+    }
 }
